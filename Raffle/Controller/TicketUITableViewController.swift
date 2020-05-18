1009129dc9f36aada07ca73f253d1a60847a63ca
VinhNguyen2 <46787486+VinhNguyen2@users.noreply.github.com>
2020-05-18 14:12:48 +1000

    Add function draw winner and other function

--- Raffle/Controller/TicketUITableViewController.swift
+++ Raffle/Controller/TicketUITableViewController.swift
@@ -8,48 +8,48 @@
 
 import UIKit
 
-class TicketUITableViewController: UITableViewController {
+class TicketUITableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var tickets = [Ticket]()
     var raffleIdFromPreviousView: Int32?
-
-//testing
+    
+    //testing
     override func viewDidLoad() {
         super.viewDidLoad()
         let database : SQLiteDatabase = SQLiteDatabase(databaseName: "MyDatabase")
         tickets = database.selectAllTicket(raffleID: raffleIdFromPreviousView!)
         print(tickets.count)
-//        tickets = database.selectAllTicket(raffleID: 1)
+        //        tickets = database.selectAllTicket(raffleID: 1)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return tickets.count
     }
-
+    
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "TicketUITableViewCell", for: indexPath)
-    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TicketUITableViewCell", for: indexPath)
+        
         let ticket = tickets[indexPath.row]
         if let ticketleCell = cell as? TicketUITableViewCell
         {
-             ticketleCell.CustomerNameLabel.text = ticket.customerName
+            ticketleCell.CustomerNameLabel.text = ticket.customerName
             ticketleCell.TicketNumberLabel.text = String(ticket.ticketNumber)
-
+            
         }
         
         return cell
-    
+        
         
     }
     
@@ -60,77 +60,74 @@ class TicketUITableViewController: UITableViewController {
             
             //"Showdetailsegue"
             //"ShowRaffleDetailSegue"
-    {
-    guard let detailViewController = segue.destination as? TicketDetailViewController else
-    {
+        {
+            guard let detailViewController = segue.destination as? TicketDetailViewController else
+            {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
             guard let selectedTicketCell = sender as? TicketUITableViewCell else
-    {
+            {
                 fatalError("Unexpected sender: \( String(describing: sender))")
             }
-    guard let indexPath = tableView.indexPath(for: selectedTicketCell) else
-    {
-    fatalError("The selected cell is not being displayed by the table") }
-    let selectedTicket = tickets[indexPath.row]
+            guard let indexPath = tableView.indexPath(for: selectedTicketCell) else
+            {
+                fatalError("The selected cell is not being displayed by the table") }
+            let selectedTicket = tickets[indexPath.row]
             detailViewController.ticket = selectedTicket
         }
     }
-
     
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func drawWinner() -> Ticket?{
+        if(tickets.count > 0){
+           
+            let randomTicket = Int(arc4random_uniform(UInt32(tickets.count)))
+            
+            return tickets[randomTicket]
+        }
+        else{
+            return nil
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @IBAction func DrawingWinerButton(_ sender: Any) {
+        let tick = drawWinner()
+        let ticketNumber = tick?.ticketNumber
+        let customerName = tick?.customerName
+        //        print(tick!)
+        if(tick != nil){
+            
+            let winner = " \n Ticket Number:\n \(ticketNumber!)\n customer name:\n \(customerName!)"
+            let alert = UIAlertController(
+                title: "Winner!",
+                message: winner,
+                preferredStyle: UIAlertController.Style.alert)
+            // add an action (button)
+            alert.addAction(UIAlertAction(
+                title: "Congratulation!",
+                style: UIAlertAction.Style.cancel,
+                handler: nil ))
+            // show the alertself.
+            present(alert, animated: true, completion: nil)
+            
+            // output = responds(ticket: tick!)
+            print(tick!)
+            //update winner information
+         //   let database : SQLiteDatabase = SQLiteDatabase(databaseName: "MyDatabase")
+          //  database.insert(raffle:Raffle(name:"RaffleA", price:23, description:"noteA", prize:500, ticketNumber:100))
+          //  database.updateRaffle(ticket: Ticket())
+           
+        }else{
+            let alert = UIAlertController(
+                title: "Warring!",
+                message: "No ticket to drawing!",
+                preferredStyle: UIAlertController.Style.alert)
+            // add an action (button)
+            alert.addAction(UIAlertAction(
+                title: "Check your raffle!",
+                style: UIAlertAction.Style.cancel,
+                handler: nil ))
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
